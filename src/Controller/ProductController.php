de9fbda2437cbf657a06cee6e5042d5631af441f
iskanderAB <iskander@keytchens.com>
2025-10-19 20:20:39 +0100

    fix: fix mercure config and disable ssl verification

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Update;
 
 final class ProductController extends AbstractController
 {
@@ -14,5 +17,20 @@ final class ProductController extends AbstractController
         return $this->render('product/index.html.twig', [
             'controller_name' => 'ProductController',
         ]);
+    }
+
+    #[Route('/add/product', name: 'app_add_product', methods: ['POST'])]
+    public function addProduct(HubInterface $hub): JsonResponse
+    {
+        for ($i = 0; $i < 5000; ++$i) {
+            $update = new Update(
+                'my-private-topic',
+                json_encode(['status' => 'OutOfStock'])
+            );
+            $hub->publish($update);
+        }
+
+        
+        return $this->json(["message" => "message send"], Response::HTTP_OK);
     }
 }
